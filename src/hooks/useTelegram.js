6d701751749589d387824788b6f5d4d19eafcc74
Harahorin <harahorinev@gmail.com>
2022-10-06 18:41:50 +0700

    init

---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const tg = window.Telegram.WebApp
+
+export function useTelegram () {
+
+    const onClose = () => {
+        tg.close()
+    }
+
+    const onToggleButton = () => {
+        if (tg.MainButton.visible) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }
+
+    return {
+        tg,
+        user: tg.initDataUnsafe?.user,
+    }
+}
